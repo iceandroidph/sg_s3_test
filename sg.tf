2c12f08d6fe2c5a3322cfa6b8021645308460408
Accurics <admin@accurics.com>
2020-10-07 23:09:07 +0000

    Auto Remediation By Accurics

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "sample_sg" {
-  name        = "sample_sg_1"
-  vpc_id      = "some_dummy_vpc"
+  name   = "sample_sg_1"
+  vpc_id = "some_dummy_vpc"
   tags = {
     Name = "acme_web_wide_open"
   }
@@ -9,7 +9,7 @@ resource "aws_security_group" "sample_sg" {
     to_port     = 22
     from_port   = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<cidr>"]
   }
 
   # outbound internet access
